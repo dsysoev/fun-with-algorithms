b7344c16b7719279d10042ef6c50b57e881eb428
Dmitry Sysoev <dsysoev@protonmail.com>
2017-12-11 13:31:33 +0300

    Docs in heapsort function was updated

--- sort/heapsort.py
+++ sort/heapsort.py
@@ -1,20 +1,35 @@
 # coding: utf-8
 
-def heapsort(ls):
+"""
+Heapsort implementation
+https://en.wikipedia.org/wiki/Heapsort
+
+Worst-case performance 	O(n * log(n))
+Best-case performance 	O(n)
+Average performance 	O(n * log(n))
+"""
+
+def heapsort(lst):
     """ Implementation of heap sort algorithm """
+    lenght = len(lst)
+    heapstart = int(lenght / 2) - 1
+    # build max heap part
+    for start in range(heapstart, -1, -1):
+        # build max heap
+        max_heapify(lst, start, lenght - 1)
+    # sorting part
+    for end in range(lenght - 1, 0, -1):
+        # zero element in heap has max value
+        # we get zero element from the heap
+        # and swap it with the end element in list
+        lst[end], lst[0] = lst[0], lst[end]
+        # restores heap properties
+        # with list from zero to end - 1
+        # storing sorted list after end - 1 element
+        max_heapify(lst, 0, end - 1)
+    return lst
 
-    l = len(ls)
-    heap_start = int(l / 2) - 1
-    for start in range(heap_start, -1, -1):
-        max_heapify(ls, start, l - 1)
-
-    for end in range(l - 1, 0, -1):
-        ls[end], ls[0] = ls[0], ls[end]
-        max_heapify(ls, 0, end - 1)
-
-    return ls
-
-def max_heapify(ls, start, end):
+def max_heapify(lst, start, end):
     """ performs permutation in a binary tree """
 
     root = start
@@ -26,12 +41,12 @@ def max_heapify(ls, start, end):
             break
         # if right value bigger than left
         # change child position to right branch
-        if child + 1 <= end and ls[child] < ls[child + 1]:
+        if child + 1 <= end and lst[child] < lst[child + 1]:
             child += 1
         # if root value lower than child
-        if ls[root] < ls[child]:
+        if lst[root] < lst[child]:
             # swap root and child elements in list
-            ls[root], ls[child] = ls[child], ls[root]
+            lst[root], lst[child] = lst[child], lst[root]
             # set new root
             root = child
         else:
@@ -39,8 +54,6 @@ def max_heapify(ls, start, end):
             break
 
 if __name__ in "__main__":
-
-    a = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7]
-
-    print('list    :', a)
-    print('heapsort:', heapsort(a), heapsort(a) == sorted(a))
+    LIST = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7]
+    print('list    :', LIST)
+    print('heapsort:', heapsort(LIST))
