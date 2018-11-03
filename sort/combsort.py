366d18bf5527cb3f0e4512473ab49b2203b80582
Dmitry Sysoev <dsysoev@protonmail.com>
2018-11-03 17:30:52 +0300

    Comb sort was added

---
+++ sort/combsort.py
@@ -0,0 +1,31 @@
+"""
+Implementing of comb sort algorith
+https://en.wikipedia.org/wiki/Comb_sort
+"""
+
+
+def combsort(lst):
+    size = len(lst)
+    # set initial gap
+    gap = size
+    swaps = True
+
+    while gap > 1 or swaps:
+        # Minimun gap is 1
+        gap = max(1, int(gap / 1.25))
+        swaps = False
+
+        for i in range(size - gap):
+            j = i + gap
+
+            if lst[i] > lst[j]:
+                # swap elements
+                lst[i], lst[j] = lst[j], lst[i]
+                swaps = True
+    return lst
+
+if __name__ in "__main__":
+
+    LIST = [1, 0, 2, 4, 5, 6, 2, 7, 9, 1, 3, 8, -1]
+    print('list        :', LIST)
+    print('comb sort   :', combsort(LIST), combsort(LIST) == sorted(LIST))
